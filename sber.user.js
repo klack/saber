5e86281f62db63f0fca519896add2d8c2f2e631b
richard-la <richard@linuxacademy.com>
2016-08-21 02:14:00 -0500

    working scroll button

--- sber.user.js
+++ sber.user.js
@@ -8,6 +8,33 @@
 // @include        https://www.safaribooksonline.com*
 // @include-jquery
 // ==/UserScript==
+// a function that loads jQuery and calls a callback function when jQuery has finished loading
+
+function addJQuery(callback) {
+  var script = document.createElement("script");
+  script.setAttribute("src", "//ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js");
+  script.addEventListener('load', function() {
+    var script = document.createElement("script");
+    script.textContent = "window.jQ=jQuery.noConflict(true);(" + callback.toString() + ")();";
+    document.body.appendChild(script);
+  }, false);
+  document.body.appendChild(script);
+}
+
+// the guts of this userscript
+function main() {
+  // Note, jQ replaces $ to avoid conflicts.
+  jQ('.sbo-menu-top').append('<div id="sber-next">-></div>');
+	jQ( "#sber-next" ).click(function() {
+	  var sber_wh = jQ( window ).height();
+	  window.scrollBy(0, sber_wh - 100);
+	});
+
+
+}
+
+// load jQuery and execute the main function
+addJQuery(main);
 
 GM_addStyle ( "                                     \
     #sbo-rt-content {                                   \
@@ -19,13 +46,13 @@ GM_addStyle ( "                                     \
 		body.sidenav { \
     	padding-left:0; !important \
 		}                                             \
+		#sber-next { \
+	    background: black; \
+	    color: white;        \
+	    font-size: 80px;    \
+	    height: 100px;         \
+	    margin-top: 50px;        \
+	    width: 100px;  \
+		} \
 	" );
 
-window.scrollBy(0, 2000);
-
-function myFunc(eventObj) {
-  window.scrollBy(0, 500);
-}
-
-var myElement = document.getElementById('container');
-myElement.addEventListener('click', myFunc);
